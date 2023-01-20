96694c8109d02619a3e34e1e7cb2234efc9e6ae9
Chen-Chi Wang <wangmasaki0318@gmail.com>
2023-01-20 13:53:46 +0000

    Just done some of the practice from the book

--- week1.py
+++ week1.py
@@ -5,5 +5,14 @@ for x in range(10):
 
 msg1 = "it is a simple code of"+" "+msg
 print(msg1.title())
+
+msg.lstrip()
+msg1.lstrip()
+
+fullmsg = f"Test: {msg}{msg1}"
+
 for y in range(18):
-    print(msg1.upper()+str(y))
+    print(fullmsg.rstrip()+str(y))
+
+
+
