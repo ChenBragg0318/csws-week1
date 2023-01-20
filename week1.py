8237510a543e50252e01760d46dbd5a9350345ab
Chen-Chi Wang <wangmasaki0318@gmail.com>
2023-01-20 13:31:54 +0000

    change a bit of lines

--- week1.py
+++ week1.py
@@ -2,4 +2,8 @@ msg = "Hello World!"
 print(msg)
 for x in range(10):
     print (msg+str(x))
-    
+
+msg1 = "it is a simple code of"+" "+msg
+print(msg1.title())
+for y in range(18):
+    print(msg1.upper()+str(y))
